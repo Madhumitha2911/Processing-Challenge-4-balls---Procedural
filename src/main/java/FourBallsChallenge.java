740d48af43d9fb2dc6f01813abd34f02b60596b4
Madhumitha M <madhumitha.m@thoughtworks.com>
2021-05-22 20:03:48 +0530

    Setup screen width and height

---
+++ src/main/java/FourBallsChallenge.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class FourBallsChallenge extends PApplet {
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
+    public static void main(String[] args) {
+        PApplet.main("FourBallsChallenge",args);
+    }
+
+    @Override
+    public void settings() {
+        size(WIDTH, HEIGHT);
+    }
+
+}
