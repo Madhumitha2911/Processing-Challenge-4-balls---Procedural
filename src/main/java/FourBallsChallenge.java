2cbc33e8bb754f686ae6f934e901dd4e4b780b89
Madhumitha M <madhumitha.m@thoughtworks.com>
2021-05-22 20:29:50 +0530

    fix position of balls on the screen

--- src/main/java/FourBallsChallenge.java
+++ src/main/java/FourBallsChallenge.java
@@ -4,6 +4,7 @@ public class FourBallsChallenge extends PApplet {
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
+    public static final int DIAMETER = 15;
 
     public static void main(String[] args) {
         PApplet.main("FourBallsChallenge",args);
@@ -14,4 +15,16 @@ public class FourBallsChallenge extends PApplet {
         size(WIDTH, HEIGHT);
     }
 
+
+    @Override
+    public void draw() {
+        ellipse(0, ballHeight(4),DIAMETER,DIAMETER);
+        ellipse(0, ballHeight(3),DIAMETER,DIAMETER);
+        ellipse(0, ballHeight(2),DIAMETER,DIAMETER);
+        ellipse(0, ballHeight(1), DIAMETER,DIAMETER);
+    }
+
+    private float ballHeight(int ballNo) {
+        return (HEIGHT * ballNo) / 5;
+    }
 }
