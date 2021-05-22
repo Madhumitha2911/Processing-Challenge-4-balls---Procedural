a1fee4cf8e36d933ca525e4a4f3d363c81c1fbbc
Madhumitha M <madhumitha.m@thoughtworks.com>
2021-05-22 20:45:16 +0530

    set speeds for each of the balls

--- src/main/java/FourBallsChallenge.java
+++ src/main/java/FourBallsChallenge.java
@@ -5,6 +5,10 @@ public class FourBallsChallenge extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 15;
+    int ballOnePosition =0;
+    int ballTwoPosition =0;
+    int ballThreeEPosition =0;
+    int ballFourPosition =0;
 
     public static void main(String[] args) {
         PApplet.main("FourBallsChallenge",args);
@@ -18,13 +22,17 @@ public class FourBallsChallenge extends PApplet {
 
     @Override
     public void draw() {
-        ellipse(0, ballHeight(4),DIAMETER,DIAMETER);
-        ellipse(0, ballHeight(3),DIAMETER,DIAMETER);
-        ellipse(0, ballHeight(2),DIAMETER,DIAMETER);
-        ellipse(0, ballHeight(1), DIAMETER,DIAMETER);
+        ellipse(ballFourPosition, ballHeight(4),DIAMETER,DIAMETER);
+        ballFourPosition+=4;
+        ellipse(ballThreeEPosition, ballHeight(3),DIAMETER,DIAMETER);
+        ballThreeEPosition+=3;
+        ellipse(ballTwoPosition, ballHeight(2),DIAMETER,DIAMETER);
+        ballTwoPosition+=2;
+        ellipse(ballOnePosition, ballHeight(1), DIAMETER,DIAMETER);
+        ballOnePosition++;
     }
 
     private float ballHeight(int ballNo) {
-        return (HEIGHT * ballNo) / 5;
+        return (float) ((HEIGHT * ballNo) / 5.0);
     }
 }
